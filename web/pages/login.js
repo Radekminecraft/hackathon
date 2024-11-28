f8fb16d1f7aa4b5594640838090e2d0f0f5e4c6e
Quotepy <abcd321hgfe@gmail.com>
2024-11-28 16:10:49 +0100

    sending user and pass func

--- web/pages/login.js
+++ web/pages/login.js
@@ -1,30 +1,34 @@
 import Header from "@/components/header";
 import React from "react";
 export default function Login() {
-    var user = document.getElementById("username");
-    var pass = document.getElementById("password");
-    const url = "localhost:8000/logcheck";
-    
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ "user": user , "pass": pass})
-    })
+
+    function submit(e){
+        e.preventDefault()
+        var user = document.getElementById("username");
+        var pass = document.getElementById("password");
+        const url = "localhost:8000/logcheck";
+        fetch(url, {
+            
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: [ user, pass]
+        })
+    }
 
     return (
         <>
         <Header></Header>
         <div id="logform-div" action="Login()">
-            <form id="logform-form">
-                <label for="username">Username</label><br></br>
+            <form onSubmit={e => submit(e)} id="logform-form">
+                <label htmlFor="username">Username</label><br></br>
                 <input type="text" id="username" name="username"></input><br></br>
 
                 <label for="password">Password</label><br></br>
                 <input type="password" id="password" name="password"></input><br></br>
-                <div class="spacer-20"></div>
+                <div className="spacer-20"></div>
                 <input id="logform-submit" type="submit" value="Submit"></input>
             </form>
         </div>
