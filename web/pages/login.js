07a5222aa1efbf428a2a63987c85331d0b4dafbe
Quotepy <abcd321hgfe@gmail.com>
2024-11-28 15:52:46 +0100

    login form

--- web/pages/login.js
+++ web/pages/login.js
@@ -1,10 +1,23 @@
 import Header from "@/components/header";
 import React from "react";
 export default function Login() {
+    var user = document.getElementById("username");
+    var pass = document.getElementById("password");
+    const url = "localhost:8000/logcheck";
+    
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "user": user , "pass": pass})
+    })
+
     return (
         <>
         <Header></Header>
-        <div id="logform-div">
+        <div id="logform-div" action="Login()">
             <form id="logform-form">
                 <label for="username">Username</label><br></br>
                 <input type="text" id="username" name="username"></input><br></br>
