f1a73fdaa189f924f8e4a3d05c0f640b635bb7f9
divear <lukyn.odehnal333@gmail.com>
2024-11-28 09:57:39 +0100

    mnammnam:

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,15 +1,24 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
+  const [pocet, setPocet] = useState(0)
+  useEffect(()=>{
+    setPocet(pocet+1)
+  },[])
   return (
     <>
       <h1 className="text-black text-6xl text-center m-4 font-mono">
-        Počítaní lidí v panelovém domě
+        {/*Počítaní lidí v panelovém domě*/}
+        projekt name
       </h1>
       <p className="text-red-600">
-
+          Počet lidí: {pocet}        
+      </p>
+      <p className="text-red-600">
+          Posledni lidi: {pocet}        
       </p>
     </>
   );
