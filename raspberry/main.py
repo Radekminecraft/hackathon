e5cfb0a76fcb45b5ce346dbad02cd2dcfbf3e6bc
Radekminecraft <radomirvondra@gmail.com>
2024-11-28 23:21:19 +0100

    Sending animal count to backend

--- raspberry/main.py
+++ raspberry/main.py
@@ -7,10 +7,11 @@ import requests
 import time
 
 model = YOLO('./trained/yolo11n.pt')
-
+UPDATETIME = 5
 mfs = True
 while mfs:
     response = requests.get("http://10.10.1.52:5000/")
+    animalCount = 0
     if response.status_code == 200:
         img_array = np.asarray(bytearray(response.content), dtype=np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
@@ -26,15 +27,17 @@ while mfs:
                 w, h = x2 - x1, y2 - y1
                 
                 conf = math.ceil((box.conf[0] * 100)) / 100
-                
-                cvzone.cornerRect(img, (x1, y1, w, h))
-                cv2.putText(img, f'{conf}', (max(0, x1), max(35, y1)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                if conf >= 0.7:
+                    cvzone.cornerRect(img, (x1, y1, w, h))
+                    cv2.putText(img, f'{conf}', (max(0, x1), max(35, y1)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                    animalCount += 1
         
         cv2.imshow("skafsl", img)
+        requests.post("https://hackathon-uygp.onrender.com/ohrady/1/zvirata", json={'pocet':f'{animalCount}'})
         if cv2.waitKey(1) == 27:
             mfs = False
 
     else:
         print("Failed to retrieve image from the Flask server.")
-    time.sleep(0.1)
+    time.sleep(UPDATETIME)
