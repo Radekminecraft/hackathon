c7dde89d367ab2748258089515d37e27c32341a7
patricino <161133826+patricinocz@users.noreply.github.com>
2024-11-29 10:46:38 +0100

    final touch
    im honored to do the final touch on this one

--- web/components/footer.js
+++ web/components/footer.js
@@ -10,14 +10,14 @@ function Footer() {
             <img src="/rabi.svg" className="img"></img>
             </div>
             <div className={styles.contacts}>
-                <a className={styles.contact}>+420 606 659 995</a>
-                <a className={styles.contact}>trackimo@company.com</a>
+                <img src="/phone.svg" className="imgi"></img><a className={styles.contacti}>+420 606 659 995</a>
+                <img src="/mail.svg" className="imgi"></img><a className={styles.contacti}>trackimo@company.com</a>
             </div>
             <div className={styles.links}>
                 <img src="/facebook.svg" className={styles.link}></img>
                 <a href="https://www.instagram.com/lukas_odehnal/"><img src="/instagram.svg" className={styles.link}></img></a>
                 <a href="https://github.com/patricinocz"><img src="/github.svg" className={styles.link}></img></a>
-                <img src="/youtube.svg" className={styles.link}></img>
+                <a href="https://www.youtube.com"><img src="/youtube.svg" className={styles.link}></img></a>
                 <img src="/tiktok.svg" className={styles.link}></img>
                 <img src="/twitter-x.svg" className={styles.link}></img>
                 <img src="/linkedin.svg" className={styles.link}></img>
